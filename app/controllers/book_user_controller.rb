bd325a59a9905ef34601b551d33aabe63175c318
Nadia M <nadmyzul@gmail.com>
2024-03-04 12:27:31 +0800

    seventh commit (trying on vs code) - write a custom helper for home so that admin has Mr. and others has gender neutral Mt.

--- app/controllers/book_user_controller.rb
+++ app/controllers/book_user_controller.rb
@@ -1,10 +1,24 @@
 class BookUserController < ApplicationController
   def index
+    @bookUser = BookUser.all
   end
 
   def create
+    @bookUser = BookUser.create(book_user_params)
   end
 
   def update
+
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book_user
+      @bookUser = BookUser.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def book_user_params
+      params.require(:book_users).permit(:user_id, :book_id)
+    end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def format_name(user)
+    if user.admin == true
+      "Back Mr. #{user.full_name}"
+    else
+      "Mt. #{user.full_name}"
+    end
+  end
 end
