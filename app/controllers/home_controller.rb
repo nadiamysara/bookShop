9ed18a12596c5e82d1d99d48b84e76cb9ed87a8f
nadiamysara <nadia.mysara1999@yahoo.com>
2024-04-29 15:40:39 +0800

    generate multiple splide carousel with loop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,6 @@ class HomeController < ApplicationController
   def index
     @books = Book.all
     @latests = @books.limit(5).reverse_order
-    @fictions = Book.where(id:BookGenre.where(genre_id:1).select(:book_id))
-    @nonfictions = Book.where(id:BookGenre.where(genre_id:2).select(:book_id))
-    # if user_signed_in?
-    #   session[:user_id] = current_user.id
-    # end
-    # if user_signed_in?
-    #   cookies[:user_id] = current_user.id
-    # end
+    @genres = Genre.all
   end
 end
--- app/javascript/splide_tool.js
+++ app/javascript/splide_tool.js
@@ -1,57 +1,59 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-    // Fiction
-    new Splide('#splideFiction', {
-        type: 'loop',
-        autoplay: true,
-        arrows: true,
-        perPage: 5,
-        breakpoints: {
-          380: {
-            perPage: 1,
-          },
-          540: {
-            perPage: 2,
-          },
-          720: {
-            perPage: 3,
-          },
-          960: {
-            perPage: 4,
-          },
-        }
-      }).mount();
+  var elms = document.getElementsByClassName( 'carouselGenre' );
+  for ( var i = 0; i < elms.length; i++ ) {
+    var genre = new Splide( elms[ i ] , {
+      type: 'loop',
+      autoplay: true,
+      arrows: true,
+      perPage: 5,
+      breakpoints: {
+        380: {
+          perPage: 1,
+        },
+        540: {
+          perPage: 2,
+        },
+        720: {
+          perPage: 3,
+        },
+        960: {
+          perPage: 4,
+        },
+      }
+    })
+    genre.on( 'overflow', function ( isOverflow ) {
+      // Reset the carousel position
+      genre.go( 0 );
+    
+      genre.options = {
+        arrows    : isOverflow,
+        pagination: isOverflow,
+        drag      : isOverflow,
+        clones    : isOverflow ? undefined : 0, // Toggle clones
+      };
+    } );
+    genre.mount()
+  }
 
-    // Non-fiction
-    var splide = new Splide('#splideNonFiction', {
-        type: 'loop',
-        autoplay: true,
-        arrows: true,
-        perPage: 5,
-        breakpoints: {
-          380: {
-            perPage: 1,
-          },
-          540: {
-            perPage: 2,
-          },
-          720: {
-            perPage: 3,
-          },
-          960: {
-            perPage: 4,
-          },
-        }
-      })
-      splide.on( 'overflow', function ( isOverflow ) {
-        // Reset the carousel position
-        splide.go( 0 );
-      
-        splide.options = {
-          arrows    : isOverflow,
-          pagination: isOverflow,
-          drag      : isOverflow,
-          clones    : isOverflow ? undefined : 0, // Toggle clones
-        };
-      } );
-      splide.mount()
-  } );
+  // Non-fiction 
+  /*new Splide('#splideNonFiction', {
+    type: 'loop',
+    autoplay: true,
+    arrows: true,
+    perPage: 5,
+    breakpoints: {
+      380: {
+        perPage: 1,
+      },
+      540: {
+        perPage: 2,
+      },
+      720: {
+        perPage: 3,
+      },
+      960: {
+        perPage: 4,
+      },
+    }
+  }).mount() */
+} );
