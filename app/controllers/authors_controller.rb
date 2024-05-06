07848402d7be251d105c01c0360f2768eb7209a5
nadiamysara <nadia.mysara1999@yahoo.com>
2024-05-06 12:57:47 +0800

    finishing touches ~ Author index sort by name ~ hyperlink class added ~ Show for Author Genre n Book's top has navlink + Genre search add desc

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,7 +7,7 @@ class AuthorsController < ApplicationController
 
   # GET /authors or /authors.json
   def index
-    @authors = Author.all
+    @authors = Author.all.order(:name)
   end
 
   # GET /authors/1 or /authors/1.json
