1b92b1eb9f0cde3545a5d59dd4cb47d8860480b4
nadiamysara <nadia.mysara1999@yahoo.com>
2024-05-06 10:24:49 +0800

    active storage author potrait + add potrait active storage to Author ~ noCoverImage to noImage - assets/books

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -69,6 +69,6 @@ class AuthorsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def author_params
-      params.require(:author).permit(:name, :dec)
+      params.require(:author).permit(:name, :dec, :potrait)
     end
 end
